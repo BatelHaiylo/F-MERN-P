8fe56401fc045d3ec6f868002781d64de42ba9d3
Batel Haiylo <inpo686@gmail.com>
2022-12-06 00:10:30 +0200

    done

--- client/src/components/features/Charts/GeoChart.component.jsx
+++ client/src/components/features/Charts/GeoChart.component.jsx
@@ -15,17 +15,6 @@ export default function GeoChart({items}) {
     {6: 7291600}
   ]
 
-  // const data = [
-  //   ["Country", "Popularity"],
-  //   ["Germany", 200],
-  //   ["United States", 300],
-  //   ["Brazil", 400],
-  //   ["Canada", 500],
-  //   ["France", 600],
-  //   ["RU", 700],
-  //   ["Israel",1000]
-  // ];
-
     console.log("new:",  [[...setup],...items?.slice(0,6).map((obj,i )=> [obj=obj.country.country, population[i][i]])]);
   
   const data = [[...setup],...items?.slice(0,6).map((obj,i )=> [obj=obj.country.country, population[i][i]])];
